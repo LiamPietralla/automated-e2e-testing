a3159fc233191daa057bf897cf11ea15061f9a79
Liam Pietralla <liam.pietralla@gmail.com>
2025-01-29 14:26:45 +1100

    failing test check

--- e2e/cypress/e2e/spec.cy.js
+++ e2e/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('counter spec', () => {
     cy.visit('/')
     cy.contains('Current Count: 0');
     cy.get('button').contains('Increment').click()
-    cy.contains('Current Count: 1');
+    cy.contains('Current Count: 10');
   });
 
   it('can decrement', () => {
